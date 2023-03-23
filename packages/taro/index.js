14fed3dab5dde7a30dba447755f87b9f57dcc2fd
kongwh <piaoxyx@gmail.com>
2023-03-23 14:28:12 +0800

    chore: 0.0.8

--- packages/taro/index.js
+++ packages/taro/index.js
@@ -2,8 +2,8 @@ module.exports = {
   extends: [
     require.resolve('@kong-dev/eslint-config-base/base'),
     'eslint:recommended',
-    'taro/react',
     'plugin:@typescript-eslint/recommended',
+    'taro/react',
     require.resolve('@kong-dev/eslint-config-base/rules'),
   ],
 }
